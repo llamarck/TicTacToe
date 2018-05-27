0551fdf00ed23dbcd93e806ea66e6e43ce16aff8
Leonis Lamarck <lamarck.ti@gmail.com>
2018-05-26 23:58:53 -0300

    Adição de EventListeners

--- js/TicTacToe.js
+++ js/TicTacToe.js
@@ -2,6 +2,16 @@ function createBoard(){
   var board = [];
 }
 
+<img src="path" onclick>
+
+function (){
+  var id;
+  for(var i = 1; i < 10; i++){
+    id = 'block' + i;
+    document.getElementById(id).addEventListener("click", changePic());
+  }
+}
+
 function fillBoard(){
   var id;
   for(var i = 1; i < 10; i++){
