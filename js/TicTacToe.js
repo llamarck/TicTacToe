73eeb9c637f68c729ab69e5d2f7991a0507baa8a
Leonis Lamarck <lamarck.ti@gmail.com>
2018-05-27 23:46:32 -0300

    Adição de eventos para alteração das imagens ao click

--- js/TicTacToe.js
+++ js/TicTacToe.js
@@ -1,25 +1,27 @@
+var flag = 0;
+
 function createBoard(){
   var board = [];
-}
-
-<img src="path" onclick>
-
-function (){
-  var id;
-  for(var i = 1; i < 10; i++){
-    id = 'block' + i;
-    document.getElementById(id).addEventListener("click", changePic());
-  }
 }
 
 function fillBoard(){
   var id;
   for(var i = 1; i < 10; i++){
     id = 'block' + i;
-    document.getElementById(id).innerHTML = "<img src='empty.jpg'>";
+    document.getElementById(id).innerHTML = "<img src='images/empty.jpg'>";
     console.log(id);
   }
   //document.getElementById();
 }
 
 fillBoard();
+
+function reply_click(clicked_id){
+  if (flag == 0){
+    document.getElementById(clicked_id).innerHTML = "<img src='images/o.jpg'>";
+    flag = 1;
+  }else if(flag == 1){
+    document.getElementById(clicked_id).innerHTML = "<img src='images/x.jpg'>";
+    flag = 0;
+  }
+}
