57636da9c698306f9c67c7dc9769a2ce3b0309ba
Leonis Lamarck <lamarck.ti@gmail.com>
2018-05-30 18:49:05 -0300

    Adição de funções para jogadas; protótipo do mapeamento de tabuleiro

--- js/TicTacToe.js
+++ js/TicTacToe.js
@@ -1,20 +1,39 @@
 var flag = 0;
+var board = [];
+var PlayerOne, PlayerTwo;
 
-function createBoard(){
-  var board = [];
-}
-
-function play(index, fig){
-  var board = createBoard();
-  board[index] = fig;
+function setNames(PlayerOne, PlayerTwo){
+  PlayerOne = document.getElementById("PlayerOne").innerText;
+  PlayerTwo = document.getElementById("PlayerTwo").innerText;
 }
 
 function checkBoard(){
-  if(board[0] && board[1] && board[2] == 0 || board[0] && board[3] && board[7] || board[0] && board[4] && board[8] || board[0] && board[1] && board[2]){
-    alert("O ganhou!");
-  }else if(){
-
+  if(board[0] == 0 && board[1] == 0 && board[2] == 0 ||
+     board[3] == 0 && board[4] == 0 && board[5] == 0 ||
+     board[6] == 0 && board[7] == 0 && board[8] == 0 ||//última horizontal
+     board[0] == 0 && board[3] == 0 && board[6] == 0 ||
+     board[1] == 0 && board[4] == 0 && board[7] == 0 ||
+     board[2] == 0 && board[5] == 0 && board[8] == 0 ||//última vertical
+     board[0] == 0 && board[4] == 0 && board[8] == 0 ||
+     board[2] == 0 && board[4] == 0 && board[6] == 0){
+    alert("0 ganhou!");
+    fillBoard();
+  }else if(board[0] == 1 && board[1] == 1 && board[2] == 1 ||
+     board[3] == 1 && board[4] == 1 && board[5] == 1 ||
+     board[6] == 1 && board[7] == 1 && board[8] == 1 ||//última horizontal
+     board[0] == 1 && board[3] == 1 && board[6] == 1 ||
+     board[1] == 1 && board[4] == 1 && board[7] == 1 ||
+     board[2] == 1 && board[5] == 1 && board[8] == 1 ||//última vertical
+     board[0] == 1 && board[4] == 1 && board[8] == 1 ||
+     board[2] == 1 && board[4] == 1 && board[6] == 1){
+    alert("X ganhou!");
+    fillBoard();
   }
+}
+
+function play(index, fig){
+  board[index] = fig;
+  checkBoard();
 }
 
 function fillBoard(){
@@ -24,9 +43,8 @@ function fillBoard(){
     document.getElementById(id).innerHTML = "<img src='images/empty.jpg'>";
     console.log(id);
   }
-  //document.getElementById();
 }
-createBoard();
+
 fillBoard();
 
 function reply_click(clicked_id){
@@ -35,14 +53,14 @@ function reply_click(clicked_id){
   if (flag == 0){
     document.getElementById(clicked_id).innerHTML = "<img src='images/o.jpg'>";
     document.getElementById(clicked_id).removeAttribute("onclick");
-    index = clicked_id.length - 1;
+    index = clicked_id.slice(-1);
     fig = 0;
     play(index, fig);
     flag = 1;
   }else if(flag == 1){
     document.getElementById(clicked_id).innerHTML = "<img src='images/x.jpg'>";
     document.getElementById(clicked_id).removeAttribute("onclick");
-    index = clicked_id.length - 1;
+    index = clicked_id.slice(-1);
     fig = 1;
     play(index, fig);
     flag = 0;
